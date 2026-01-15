cdde7bd45fa0d043b865bab16ea1e8ccd24bc067
Matthew Campbell <matt@moderne.io>
2026-01-14 21:20:05 -0500

    Revert to starting point for migration practice

--- src/test/java/com/example/ecom/notification/NotificationServiceIntegrationTest.java
+++ src/test/java/com/example/ecom/notification/NotificationServiceIntegrationTest.java
@@ -2,10 +2,13 @@ package com.example.ecom.notification;
 
 import com.example.ecom.common.event.OrderCreatedEvent;
 import com.example.ecom.notification.listener.NotificationListener;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class NotificationServiceIntegrationTest {
 
